fca446732291a973567199d78ecaaf3664e056c3
Balibey <kemalbali@hotmail.com>
2022-04-19 01:17:34 +0300

    Swagger aktif edildi. Configuration settings ok.

--- projectboard/src/main/java/com/projectboard/projectboard/ProjectboardApplication.java
+++ projectboard/src/main/java/com/projectboard/projectboard/ProjectboardApplication.java
@@ -2,12 +2,24 @@ package com.projectboard.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
 @SpringBootApplication
+@EnableSwagger2
 public class ProjectboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectboardApplication.class, args);
 	}
 
+	@Bean
+	public Docket docket() {
+		return new Docket (DocumentationType.SWAGGER_2).select().build();
+	}
 }
